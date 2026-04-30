22a252d4d821955fc662df92a8497483117ae3b2
Mikołaj Florczak <84631301+florczaq@users.noreply.github.com>
2026-04-30 15:48:40 +0200

    Add real-time hand detection using MediaPipe and update Python SDK version

--- main.py
+++ main.py
@@ -1,24 +1,38 @@
-import cv2 as cv
+import cv2
+import mediapipe as mp
 
-cv.namedWindow('frame', cv.WINDOW_AUTOSIZE)
+mp_hands = mp.solutions.hands
+mp_draw = mp.solutions.drawing_utils
 
-if __name__ == '__main__':
-    cap = cv.VideoCapture(0)
-    if not cap.isOpened():
-        print("Cannot open camera")
-        exit()
+cv2.namedWindow('frame', cv2.WINDOW_AUTOSIZE)
+
+
+
+cap = cv2.VideoCapture(0)
+
+with mp_hands.Hands(
+        max_num_hands=2,
+        min_detection_confidence=0.5,
+        min_tracking_confidence=0.5) as hands:
     while True:
         ret, frame = cap.read()
-
         if not ret:
-            print("Can't receive frame (stream end?). Exiting ...")
             break
-        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-        cv.imshow('frame', gray)
-        k = cv.waitKey(1)
-        if cv.getWindowProperty('frame', cv.WND_PROP_VISIBLE) < 1:
+
+        img_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results = hands.process(img_rgb)
+
+        if results.multi_hand_landmarks:
+            for hand in results.multi_hand_landmarks:
+                mp_draw.draw_landmarks(frame, hand, mp_hands.HAND_CONNECTIONS)
+
+        cv2.imshow("frame", frame)
+
+        k = cv2.waitKey(1)
+        if cv2.getWindowProperty('frame', cv2.WND_PROP_VISIBLE) < 1:
             break
         if k == 27:
             break
-    cap.release()
-    cv.destroyAllWindows()
+
+cap.release()
+cv2.destroyAllWindows()
